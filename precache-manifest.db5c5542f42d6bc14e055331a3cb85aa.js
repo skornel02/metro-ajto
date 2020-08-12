554b6a2d1900003db7ca5c0138ba9b500d646ccc
skornel02 <stefankornel02@gmail.com>
2020-08-12 20:21:54 +0000

    Deploying to gh-pages from  @ cc6ca50bdf0033194471190511673944e8b2fe92 🚀

---
+++ precache-manifest.db5c5542f42d6bc14e055331a3cb85aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e8a926e05e952a228567ef4c79486f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd3d423bbc8f4d6921b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d8532393cbd14b8b9555",
+    "url": "/static/js/2.945a260a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.945a260a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd3d423bbc8f4d6921b",
+    "url": "/static/js/main.84a0296e.chunk.js"
+  },
+  {
+    "revision": "2ef27bb6395f0d8a7fa9",
+    "url": "/static/js/runtime-main.14c8029a.js"
+  }
+]);
