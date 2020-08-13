fd2b54c30d44f94be40a98cfca8f9036df70ac20
hgergely03 <47475514+hgergely03@users.noreply.github.com>
2020-08-13 00:45:47 +0000

    Deploying to gh-pages from  @ 501af57fa52ae8412c4907e8fd0063ee8079de62 🚀

---
+++ precache-manifest.7d6243d2098cbcb044bca0e4aeb71467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9a2b7dfa706b3f1e6eb031b4a1d88f",
+    "url": "https://skornel02.github.io/metro-ajto/index.html"
+  },
+  {
+    "revision": "b0a4b9fcb222280652e1",
+    "url": "https://skornel02.github.io/metro-ajto/static/css/main.aa86c1c3.chunk.css"
+  },
+  {
+    "revision": "d8532393cbd14b8b9555",
+    "url": "https://skornel02.github.io/metro-ajto/static/js/2.945a260a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://skornel02.github.io/metro-ajto/static/js/2.945a260a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a4b9fcb222280652e1",
+    "url": "https://skornel02.github.io/metro-ajto/static/js/main.e5b0a2c0.chunk.js"
+  },
+  {
+    "revision": "444913965a9868f83b8b",
+    "url": "https://skornel02.github.io/metro-ajto/static/js/runtime-main.ee64c131.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://skornel02.github.io/metro-ajto/precache-manifest.16ecfc9149bed03764d44a596cfea075.js"
+  "https://skornel02.github.io/metro-ajto/precache-manifest.7d6243d2098cbcb044bca0e4aeb71467.js"
 );
 
 self.addEventListener('message', (event) => {
